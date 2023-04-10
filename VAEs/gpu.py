7e5c2dfb37e1b31cecb93ee84b5021e7a567da91
Daisuke Yamada <daisukeyamada@Daisukes-MacBook-Pro.local>
2023-04-10 13:21:33 +0200

    changed gpu setting

--- VAEs/gpu.py
+++ VAEs/gpu.py
@@ -10,22 +10,10 @@ def check_gpu():
     import timeit, functools
     print('CPU (s):')
     cpu_time = timeit.Timer(functools.partial(run_cpu, cpu_device))
-    print(cpu_time.timeit(1))
+    print(cpu_time.timeit(10))
     print('GPU (s):')
     gpu_time = timeit.Timer(functools.partial(run_gpu, gpu_device))
-    print(gpu_time.timeit(1))
-
-# def run_cpu():
-#     with tf.device('/cpu:0'):
-#         random_image_gpu = tf.random.normal((100, 100, 100, 3))
-#         net_gpu = tf.keras.layers.Conv2D(32, 7)(random_image_gpu)
-#         tf.math.reduce_sum(net_gpu)
-
-# def run_gpu():
-#     with tf.device(device_name):
-#         random_image_gpu = tf.random.normal((100, 100, 100, 3))
-#         net_gpu = tf.keras.layers.Conv2D(32, 7)(random_image_gpu)
-#         tf.math.reduce_sum(net_gpu)
+    print(gpu_time.timeit(10))
     
 x_train, _ = utils.load_data()
 x_train = utils.preprocess_image_data(x_train)
@@ -37,10 +25,12 @@ def run_cpu(cpu_device):
     with tf.device(cpu_device):
         for x_batch in x_train:
             model.train_per_batch(net, x_batch, cpu_device)
+            break
 
 def run_gpu(gpu_device):
     with tf.device(gpu_device):
         for x_batch in x_train:
             model.train_per_batch(net, x_batch, gpu_device)
+            break
 
 check_gpu()
