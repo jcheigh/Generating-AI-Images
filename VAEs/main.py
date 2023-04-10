1379521f2555d5e9c2397e1d075e14302c7fd184
Daisuke Yamada <daisukeyamada@Daisukes-MacBook-Pro.local>
2023-04-10 08:07:35 +0200

    model working for digits

--- VAEs/main.py
+++ VAEs/main.py
@@ -1,7 +1,19 @@
+import tensorflow as tf
+
 import utils
 import model 
+import gpu
+
+# config hyperprameters
+epochs = 10
+batch_size = 32
+latent_dim = 2
+
 
 def main():
+    # test and get Colab gpu
+    device_name = gpu.test_gpu()
+
     # get data
     x_train, x_test = utils.load_data()
 
@@ -10,10 +22,12 @@ def main():
     x_test = utils.preprocess_image_data(x_test)
 
     # split data into batches
-    x_train = utils.split_batch(image_data=x_train, batch_size=32)
-    x_test = utils.split_batch(image_data=x_test, batch_size=32)
-    #print(np.shape(list(x_train)))
+    x_train = utils.split_batch(image_data=x_train, batch_size=batch_size)
+    x_test = utils.split_batch(image_data=x_test, batch_size=batch_size)
 
-    vae = model.VAE(latent_dim=2)
+    # define and train model
+    with tf.device(device_name=device_name):
+        vae = model.VAE(latent_dim=latent_dim)
+        model.train(model=vae, epochs=epochs, x=x_train, x_test=x_test)
 
 main()
